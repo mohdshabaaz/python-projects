910f1e649c89652b7c48ed73735d43a2845875dc
mohdshabaaz <mohdshabaaz1919@gmail.com>
2021-07-31 16:27:39 +0530

    i

--- sqr,sqrt.py
+++ sqr,sqrt.py
@@ -1,31 +1,30 @@
 from math import *
 s=10
 while s>1:
-  try:
-    sqr="square"
-    sqt="square root"
-    print("please type which operation you want to perform. square or square root?")
-    if opr!=(sqr,sqrt):
-             print("error")	
-        
-    opr=str(input())
+	try:
+		sqr="square"
+		sqt="square root"
+		print("please type which operation you want to perform. square or square root?")
 		
-    if opr==sqr:
-	  try:
-	     print("Type the number ")
-	     num=int(input())
-	     num1=num*num
-	     print('square of %f is %f'%(num,num1)
+	    
+		opr=str(input())
+		
+		if opr==sqr:
+				try:
+					print("Type the number ")
+					num=int(input())
+					num1=num*num
+					print('square of %f is %f'%(num, num1))
  
-	   except:
-	     print("error! please type number.")
-     elif opr==sqt:
-	    try:
-	       print("Type the number ")
-	       num2=int(input(""))
-	       num3=sqrt(num2)
-	       print("square root of %d is %d"%(num2,num3)
-	     except:
-	       print("error")
-  except:
-    print("error")
+				except:
+					print("error! please type number.")
+		elif opr==sqt:
+				try:
+					print("Type the number ")
+					num2=int(input(""))
+					num3=sqrt(num2)
+					print("square root of %f is %f" %(num2,num3))
+				except:
+					print("error")
+	except:
+		print("error")
